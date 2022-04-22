c7219db6bec3e4d5feb2a605482c0b2a5ff33e8e
Savrak Bohdan <73792196+Black-Blood@users.noreply.github.com>
2022-04-22 13:34:26 +0300

    Checker

--- Checkers/Core/Checker.cs
+++ Checkers/Core/Checker.cs
@@ -2,12 +2,19 @@
 
 public class Checker
 {
-    public Color Color { get; private set; }
-
     public bool IsKing { get; set; } = false;
 
-    public Checker(Color color)
+    public Color Color { get; private set; }
+
+    public Position Position { get; internal set; }
+
+    public List<Position> Moves { get; internal set; } = new();
+
+    public List<Checker> UnderAttackCheckers { get; internal set; } = new();
+
+    public Checker(Position position, Color color)
     {
+        Position = position;
         Color = color;
     }
 }
